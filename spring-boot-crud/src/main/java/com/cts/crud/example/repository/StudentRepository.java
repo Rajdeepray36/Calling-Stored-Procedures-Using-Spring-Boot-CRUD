b0615afc6c8284210dcef2d3e3204551c24b4050
Rajdeepray36 <99804543+Rajdeepray36@users.noreply.github.com>
2023-03-10 17:13:41 +0530

    Delete spring-boot-crud directory

--- spring-boot-crud/src/main/java/com/cts/crud/example/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.crud.example.repository;
-
-import com.cts.crud.example.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-    Student findByName(String name);
-}
-
--- spring-boot-crud/src/main/java/com/cts/crud/example/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.crud.example.repository;
-
-import com.cts.crud.example.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher,Integer> {
-    Teacher findByName(String name);
-}
-
